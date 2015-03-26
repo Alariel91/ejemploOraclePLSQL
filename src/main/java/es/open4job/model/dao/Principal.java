50804edc5c561bf2558a8043c32403240301d56e
alariel91 <monysaenzcarrero@gmail.com>
2015-03-26 13:28:10 +0100

    Modificado para que te devuelva un cursor y visualice la lista de aparcamientos.

--- src/main/java/es/open4job/model/dao/Principal.java
+++ src/main/java/es/open4job/model/dao/Principal.java
@@ -1,26 +1,47 @@
 package es.open4job.model.dao;
 
 import java.sql.CallableStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import oracle.jdbc.internal.OracleTypes;
 
 public class Principal {
 	static CallableStatement cs;
 	static SentenciasSQL conexion =new SentenciasSQL();
+	static ResultSet rs ;
 	public static void main(String[] args) {
 		
-		
 		conexion.conectarBBDD();
-		String getTituloById= "{call getTitulo(?,?)}";
+		String procursor= "{call procursor(?)}";
+		
 		try {
-			cs= conexion.getConn().prepareCall(getTituloById);
-			cs.setInt(1, 1);
-			cs.registerOutParameter(2,java.sql.Types.VARCHAR);
-			cs.execute();
+		
+			cs= conexion.getConn().prepareCall(procursor);
 			
-			String titulo = cs.getString(2);
-			System.out.println(titulo);
+			cs.registerOutParameter(1,OracleTypes.CURSOR);
+			
+			cs.executeUpdate();
+			
+			rs = (ResultSet) cs.getObject(1);
+			
+			while (rs.next()) {
+				System.out.println("Id: " + rs.getInt("ID"));
+				System.out.println("Latitud: " + rs.getFloat("LATITUD"));
+				System.out.println("Longitud: " + rs.getFloat("LONGITUD"));
+				System.out.println("Titulo: " + rs.getString("TITULO"));
+				System.out.println("Icono: " + rs.getString("ICONO"));
+				System.out.println("Tipo: " + rs.getString("TIPO"));
+			}
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
+		}finally{
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 		
 		conexion.closeBBDD();
